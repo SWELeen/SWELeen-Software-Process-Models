99114603ae058491294becf3a4555f21b9cb1980
Leen Alghamdi <leen.saleh555@gmail.com>
2024-11-04 23:04:42 +0300

    change the port to 3003

--- FullProject/server.js
+++ FullProject/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const app = express();
-const PORT = 3002;
+const PORT = 3003;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
